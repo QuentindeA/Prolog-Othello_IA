bcca3fdbad647424a1423721c415bdfe9e8319b9
Quentin de Andria <quentin.de.andria@gmail.com>
2020-10-17 21:10:44 +0200

    add test availableRow

--- othello.pl
+++ othello.pl
@@ -19,7 +19,7 @@ play(Player) :- write('Player "'), write(Player), writeln('" can not play.'), ch
 
 %Fin de partie
 %%When it is no longer possible for either player to move, the game is over.
-gameover(Winner) :- board(Board), not(moveAvailable(Board, 'b')), not(moveAvailable(Board, 'w')), findWinner(Board,Winner).
+gameover(Winner) :- board(Board), not(moveAvailable(Board, P)), findWinner(Board,Winner).
 
 
 %Trouver le gagnant, si quelqu'un a une meilleur soluce..
@@ -27,7 +27,7 @@ gameover(Winner) :- board(Board), not(moveAvailable(Board, 'b')), not(moveAvaila
 countDisk([],[],'Draw').
 countDisk([],_,'w').
 countDisk(_,[],'b').
-countDisk([X|L1],[Y|L2],Winner) :- countDisk(L1,L2,Winner).
+countDisk([_|L1],[_|L2],Winner) :- countDisk(L1,L2,Winner).
 
 %Separe les disques blancs et noirs
 %%L1 de longeur le nombre de disque noir
@@ -41,18 +41,43 @@ separate([_|Board],L1,L2) :- separate(Board,L1,L2). % autre que b ou w
 findWinner(Board,Winner) :- separate(Board,L1,L2), countDisk(L1,L2,Winner). 
 
 
-%TODO
-%A move is made by placing a disc of the player's color on the board in a position that "out-flanks" one or more of the opponent's discs.
-%A disc or row of discs is outflanked when it is surrounded at the ends by discs of the opposite color.
-%A disc may outflank any number of discs in one or more rows in any direction (horizontal, vertical, diagonal).
-moveAvailableCol(B,_).
-moveAvailableLine(B,_).
-moveAvailableDiagR(B,_).
-moveAvailableDiagL(B,_).
+%On cherche si il existe encore un mouvement possible
+%%A move is made by placing a disc of the player's color on the board in a position that "out-flanks" one or more of the opponent's discs.
+%%A disc or row of discs is outflanked when it is surrounded at the ends by discs of the opposite color.
+%%A disc may outflank any number of discs in one or more rows in any direction (horizontal, vertical, diagonal).
+%On cherche si on trouve un mouvement possible dans une ligne, puis une colonnes, puis les deux diagonales. P est le player qui a le move
+moveAvailable(Board,P) :- moveAvailableRow(Board,[],0,P).
 moveAvailable(Board,_) :- moveAvailableCol(Board,_).
-moveAvailable(Board,_) :- moveAvailableLine(Board,_).
 moveAvailable(Board,_) :- moveAvailableDiagR(Board,_).
 moveAvailable(Board,_) :- moveAvailableDiagL(Board,_).
+
+%Création d'une liste par ligne et on teste s'il existe un move est possible dans la liste
+%TODO Pas complétement correcte...
+moveAvailableRow([],B,_,P) :- moveAvailableArray(B,'start',P).
+moveAvailableRow(B2,B,8,P) :- B2\==[], moveAvailableArray(B,'start',P).
+moveAvailableRow(B,_,8,P) :- B\==[], line(B,[],0,P).
+moveAvailableRow([E|B],B2,Pos,P) :- Pos \== 8, NewPos is Pos+1, moveAvailableRow(B,[E|B2],NewPos,P).
+
+%TODO
+%Création d'une liste par colonne et test
+moveAvailableCol(_,_) :- 1=2.
+%Création d'une liste par diag hautgauche vers basdroite
+moveAvailableDiagR(_,_) :- 1=2.
+%Création d'une liste par diag hautdroite vers basgauche
+moveAvailableDiagL(_,_) :- 1=2.
+
+
+%On cherche si on trouve une place valide dans une liste
+moveAvailableArray([D|B],'start',P) :- var(D), moveAvailableArray1(B,_,P). % on a une place au début
+moveAvailableArray([D|B],'start',P) :- nonvar(D), moveAvailableArray2(B,D,P). % on a pas de place au début
+
+moveAvailableArray1([D|B],_,P) :- var(D), moveAvailableArray1(B,_,P). %tant que l on a des places libres, on les retire
+moveAvailableArray1([D|B],PrevD,P) :- nonvar(D), var(PrevD), moveAvailableArray1(B,D,P). %On trouve le premier élément
+moveAvailableArray1([D|B],PrevD,P) :- nonvar(D), nonvar(PrevD), D==PrevD, moveAvailableArray1(B,D,P). %Il existe un précédent mais meme couleur
+moveAvailableArray1([D|_],PrevD,D) :- nonvar(D), nonvar(PrevD), D\==PrevD. %Il existe un précédent et il est de couleur différente. Il existe un move
+%TODO
+moveAvailableArray2(_,_,_) :- 1=2.
+
 
 
 %TODO : Create different versions
